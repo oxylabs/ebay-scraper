f170945e4df07a721bc74beaff33634a59a9d957
OxylabsOrg <103110131+OxylabsOrg@users.noreply.github.com>
2024-10-01 12:24:23 +0300

    Update ebay_java.java

--- code examples/ebay_java.java
+++ code examples/ebay_java.java
@@ -8,7 +8,7 @@ public class Main implements Runnable {
 
     public void run() {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("source", "universal_ecommerce");
+        jsonObject.put("source", "universal");
         jsonObject.put("url", "https://www.ebay.com/itm/293608130360");
         jsonObject.put("geo_location", "United States");
 
